846568080790a2b2e5f782646f713488f1852809
MikSym <126161673+MikSym@users.noreply.github.com>
2023-04-05 09:51:27 +0200

    Update 5.5.py

--- cw5/5.5.py
+++ cw5/5.5.py
@@ -35,3 +35,33 @@ ciag.wyswietl_dane()
 ciag.policz_sume()
 
 ciag.policz_elementy(1, 3, 5)
+
+
+### 6
+class Robaczek:
+    def __init__(self, x, y, krok):
+        self.x = x
+        self.y = y
+        self.krok = krok
+
+    def idz_w_gore(self, ile_krokow):
+        self.y += ile_krokow * self.krok
+
+    def idz_w_dol(self, ile_krokow):
+        self.y -= ile_krokow * self.krok
+
+    def idz_w_lewo(self, ile_krokow):
+        self.x -= ile_krokow * self.krok
+
+    def idz_w_prawo(self, ile_krokow):
+        self.x += ile_krokow * self.krok
+
+    def pokaz_gdzie_jestes(self):
+        print("Aktualne współrzędne: x =", self.x, ", y =", self.y)
+
+        robaczek = Robaczek(0, 0, 5)
+        robaczek.idz_w_gore(2)
+        robaczek.idz_w_lewo(1)
+        robaczek.idz_w_dol(1)
+        robaczek.idz_w_prawo(3)
+        robaczek.pokaz_gdzie_jestes()
